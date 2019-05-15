70855b641ca82b8857ed8c728180d0b81512a96a
Kehinde Edward <kennyedward99@gmail.com>
2019-05-15 23:54:54 +0100

    chore(userController): update controller class

--- server/api/v1/middleware/userValidator.js
+++ server/api/v1/middleware/userValidator.js
@@ -1,72 +1,77 @@
-const valiidateSignUp = (req, res, next) => {
-  let validationMessage = '';
+class UserValidator {
+  static validateSignUp(req, res, next) {
+    let validationMessage = '';
 
-  if (!req.body.email) {
-    validationMessage += 'Email is required';
-  } else if (req.body.email && !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(req.body.email)) {
-    validationMessage += 'Invalid email: example - user@domain.com';
-  } else if (!req.body.firstName) {
-    validationMessage += 'First name is required';
-  } else if (typeof req.body.firstName !== 'string') {
-    validationMessage += 'Invalid first name';
-  } else if (!req.body.firstName.trim()) {
-    validationMessage += 'First name is empty';
-  } else if (/^[a-zA-Z]*$/.test(req.body.firstName) === false) {
-    validationMessage += 'First name can only contain alphabets.';
-  } else if ((req.body.firstName.trim().length < 3) || (req.body.firstName.trim().length > 20)) {
-    validationMessage += 'First name length should be between 3 and 20';
-  } else if (!req.body.lastName) {
-    validationMessage += 'Last name is required';
-  } else if (typeof req.body.lastName !== 'string') {
-    validationMessage += 'Invalid last name';
-  } else if (!req.body.lastName.trim()) {
-    validationMessage += 'Last name is empty';
-  } else if (/^[a-zA-Z]*$/.test(req.body.lastName) === false) {
-    validationMessage += 'Last name can only contain alphabets.';
-  } else if ((req.body.lastName.trim().length < 3) || (req.body.lastName.trim().length > 20)) {
-    validationMessage += 'Last name length should be between 3 and 20';
-  } else if (!req.body.password) {
-    validationMessage += 'password is required';
-  } else if (req.body.password !== req.body.confirmPassword) {
-    validationMessage += 'password does not match';
-  } else if (!req.body.address) {
-    validationMessage += 'Address is required';
-  } else if (typeof req.body.address !== 'string') {
-    validationMessage += 'Invalid address';
-  } else if (req.body.address.trim().length === 0) {
-    validationMessage += 'Address is empty';
-  } else if (/^[a-zA-Z0-9-.,' ]*$/.test(req.body.address) === false) {
-    validationMessage += 'Address can only contain alphabets, numbers, comma and hyphen.';
+    if (!req.body.email) {
+      validationMessage += 'Email is required';
+    } else if (req.body.email && !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(req.body.email)) {
+      validationMessage += 'Invalid email: example - user@domain.com';
+    } else if (!req.body.firstName) {
+      validationMessage += 'First name is required';
+    } else if (typeof req.body.firstName !== 'string') {
+      validationMessage += 'Invalid first name';
+    } else if (!req.body.firstName.trim()) {
+      validationMessage += 'First name is empty';
+    } else if (/^[a-zA-Z]*$/.test(req.body.firstName) === false) {
+      validationMessage += 'First name can only contain alphabets.';
+    } else if ((req.body.firstName.trim().length < 3) || (req.body.firstName.trim().length > 20)) {
+      validationMessage += 'First name length should be between 3 and 20';
+    } else if (!req.body.lastName) {
+      validationMessage += 'Last name is required';
+    } else if (typeof req.body.lastName !== 'string') {
+      validationMessage += 'Invalid last name';
+    } else if (!req.body.lastName.trim()) {
+      validationMessage += 'Last name is empty';
+    } else if (/^[a-zA-Z]*$/.test(req.body.lastName) === false) {
+      validationMessage += 'Last name can only contain alphabets.';
+    } else if ((req.body.lastName.trim().length < 3) || (req.body.lastName.trim().length > 20)) {
+      validationMessage += 'Last name length should be between 3 and 20';
+    } else if (!req.body.password) {
+      validationMessage += 'password is required';
+    } else if (!req.body.password.trim().length === 0) {
+      validationMessage += 'password cannot contain only spaces. mix with other characters';
+    } else if (req.body.password.trim().length < 5) {
+      validationMessage += 'password must include minimum of 5 characters';
+    } else if (!req.body.address) {
+      validationMessage += 'Address is required';
+    } else if (typeof req.body.address !== 'string') {
+      validationMessage += 'Invalid address';
+    } else if (req.body.address.trim().length === 0) {
+      validationMessage += 'Address is empty';
+    } else if (/^[a-zA-Z0-9-.,' ]*$/.test(req.body.address) === false) {
+      validationMessage += 'Address can only contain alphabets, numbers, comma and hyphen.';
+    } else if (req.body.address.length < 20) {
+      validationMessage += 'Address must be greater than 20 characters.';
+    }
+    return (validationMessage.length === 0) ? next()
+      : res.status(400).json({ status: 400, error: validationMessage });
   }
-  return (validationMessage.length === 0) ? next()
-    : res.status(400).json({ status: 400, error: validationMessage });
-};
 
-const valiidateLogin = (req, res, next) => {
-  let validationMessage = '';
+  static validateLogin(req, res, next) {
+    let validationMessage = '';
 
-  if (!req.body.email) {
-    validationMessage += 'Email is required';
-  } else if (req.body.email && !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(req.body.email)) {
-    validationMessage += 'Invalid email: example - user@domain.com';
-  } else if (!req.body.password) {
-    validationMessage += 'password is required';
+    if (!req.body.email) {
+      validationMessage += 'Email is required';
+    } else if (req.body.email && !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(req.body.email)) {
+      validationMessage += 'Invalid email: example - user@domain.com';
+    } else if (!req.body.password || req.body.password.trim().length === 0) {
+      validationMessage += 'password is required';
+    }
+    return (validationMessage.length === 0) ? next()
+      : res.status(400).json({ status: 400, error: validationMessage });
   }
-  return (validationMessage.length === 0) ? next()
-    : res.status(400).json({ status: 400, error: validationMessage });
-};
 
-const validateVerificationStatus = (req, res, next) => {
-  let validationMessage = '';
-  if (!req.body.status) {
-    validationMessage += 'Status is required';
+  static validateVerificationStatus(req, res, next) {
+    let validationMessage = '';
+    const status = req.body.status.trim();
+    if (!status) {
+      validationMessage += 'Status is required';
+    } else if (status.trim() !== 'verified' && status !== 'unverified') {
+      validationMessage += 'Status can only be either \'verified\', or \'unverified\'';
+    }
+    return (validationMessage.length === 0) ? next()
+      : res.status(400).json({ status: 400, error: validationMessage });
   }
-  return (validationMessage.length === 0) ? next()
-    : res.status(400).json({ status: 400, error: validationMessage });
-};
+}
 
-export default {
-  valiidateSignUp,
-  valiidateLogin,
-  validateVerificationStatus,
-};
+export default UserValidator;
--- server/api/v1/routes/usersRoute.js
+++ server/api/v1/routes/usersRoute.js
@@ -6,8 +6,8 @@ import jwt from '../middleware/jwt';
 
 const usersRouter = express.Router();
 
-usersRouter.post('/auth/signup', userValidator.valiidateSignUp, userController.userSignUp);
-usersRouter.post('/auth/login', userValidator.valiidateLogin, userController.userLogin);
-usersRouter.patch('/:userEmail/verify', jwt.validateToken, userValidator.validateVerificationStatus, userController.adminVerifyAccount);
+usersRouter.post('/auth/signup', userValidator.validateSignUp, userController.userSignUp);
+usersRouter.post('/auth/login', userValidator.validateLogin, userController.userLogin);
+usersRouter.patch('/users/:userEmail/verify', jwt.validateToken, userValidator.validateVerificationStatus, userController.adminVerifyAccount);
 
 export default usersRouter;
