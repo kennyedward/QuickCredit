d991413a5334e8f3e504b1df50afa04ac5ddb0bc
Kehinde Edward <kennyedward99@gmail.com>
2019-05-16 12:52:55 +0100

    chore(loan): refactor getAllLoan method

--- server/api/v1/routes/loanRoute.js
+++ server/api/v1/routes/loanRoute.js
@@ -11,7 +11,7 @@ loanRouter.post('/', jwt.validateToken, loanController.applyForLoan);
 loanRouter.patch('/:loanId', jwt.validateToken, checker.checkAdminStatus, loanValidator.validateLoanId, loanController.adminApproveRejectLoan);
 loanRouter.post('/:loanId/repayment', jwt.validateToken, loanValidator.validateLoanRepayment, loanController.loanRepayment);
 loanRouter.get('/:loanId/repayment', jwt.validateToken, loanValidator.validateLoanId, loanController.getRepayment);
-loanRouter.get('/', jwt.validateToken, loanController.getAllLoan);
+loanRouter.get('/', jwt.validateToken, checker.checkAdminStatus, loanController.getAllLoan);
 loanRouter.get('/:loanId', jwt.validateToken, loanValidator.validateLoanId, loanController.getALoan);
 
 export default loanRouter;
