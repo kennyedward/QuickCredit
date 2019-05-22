79d7899e21a5e113c3bd979380e0b4c21531c401
Kehinde Edward <kennyedward99@gmail.com>
2019-05-22 13:21:53 +0100

    feature(user): Implement user signup and signin
     - Add user signup functionality
     - Implement user signin functionality
     - Add endpoint integration tests
    
    [Finishes #166101608]

--- server/api/v1/controllers/userController.js
+++ server/api/v1/controllers/userController.js
@@ -1,67 +1,74 @@
 import bcrypt from 'bcrypt';
 
 import users from '../db/users';
-import idGenerator from '../helpers/IdGenerator';
 import jwt from '../middleware/jwt';
 import statusValidator from '../helpers/verifyStatus';
+import db from '../db/index';
 
 class UserController {
-  static userSignUp(req, res) {
-    const newUser = {
-      id: idGenerator(),
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      password: req.body.password,
-      address: req.body.address,
-      status: 'unverified',
-      isAdmin: false,
-      createdOn: new Date(),
-    };
-    const result = users.find(user => user.email === newUser.email);
-    if (!result) {
-      bcrypt.hash(newUser.password, 8, (err, hashPassword) => {
-        if (err) {
-          return res.status(400).json({
-            status: 400,
-            error: 'An error occured during signup. Please try again.',
-          });
-        }
-        newUser.password = hashPassword;
-        users.push(newUser);
-        const status = 201;
-        return jwt.createToken(newUser, res, status);
-      });
-    } else {
-      res.status(409).json({
+  static async userSignUp(req, res) {
+    let text = `SELECT * FROM users
+      WHERE email = $1`;
+    let values = [
+      req.body.email,
+    ];
+    const { rows } = await db.query(text, values);
+    if (rows[0]) {
+      return res.status(409).json({
         status: 409,
-        error: 'User already exists',
+        error: 'User with the email already exists',
       });
     }
+    const hashPassword = await bcrypt.hash(req.body.password, 8);
+    values = [
+      req.body.email,
+      req.body.firstName,
+      req.body.lastName,
+      hashPassword,
+      req.body.address,
+      'unverified',
+      false,
+      new Date(),
+    ];
+    text = `INSERT INTO
+          users(email, firstName, lastName, password, address, status, isAdmin, createdOn)
+          VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+          returning *`;
+    const queryUserTable = await db.query(text, values);
+    const { password, ...createdUser } = queryUserTable.rows[0];
+    const token = await jwt.createToken(createdUser);
+    return res.status(201).json({
+      status: 201,
+      data: { token, ...createdUser },
+    });
   }
 
-  static userLogin(req, res) {
-    const user = {
-      email: req.body.email,
-      password: req.body.password,
-    };
-    const result = users.find(registeredUser => registeredUser.email === user.email);
-    if (!result) {
+  static async userLogin(req, res) {
+    const text = `SELECT * FROM users
+      WHERE email = $1`;
+    const returnedUser = await db.query(text, [req.body.email]);
+    if (!returnedUser.rows[0]) {
       res.status(404).json({
         status: 404,
         error: 'User not found',
       });
     } else {
-      bcrypt.compare(user.password, result.password, (err, match) => {
+      bcrypt.compare(req.body.password, returnedUser.rows[0].password, (err, match) => {
         if (err) {
           return res.status(401).json({
             status: 401,
             error: 'An error occured during log in',
           });
         }
-        const status = 200;
-        return match ? jwt.createToken(result, res, status)
-          : res.status(401).json({ status: 401, error: 'Auth failed' });
+        if (!match) {
+          return res.status(401).json({ status: 401, error: 'Auth failed' });
+        }
+        const token = jwt.createToken(returnedUser.rows[0]);
+        const { password, ...user } = returnedUser.rows[0];
+        return res.status(200).json({
+          status: 200,
+          data: { token, ...user },
+        });
       });
     }
   }
